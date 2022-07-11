f1d338deb158473557da8a7f6484f6a111aa22a1
raycast5 <ray.j.colon@gmail.com>
2022-07-11 18:01:37 +0000

    Updated _printf

--- _printf.c
+++ _printf.c
@@ -22,11 +22,7 @@ int _printf(const char *const format, ...)
 	{
 		for (i = 0; format[i] != '\0'; i++)
 		{
-<<<<<<< HEAD
 			if (format[i] == '%')
-=======
-			if (format[i] == '%' || format[i + 1] != '%')
->>>>>>> 9fa140adb43ffc96c77b1b8fc99ed6875cd3cd15
 			{
 				i++;
 				func = matchf(format[i]);
