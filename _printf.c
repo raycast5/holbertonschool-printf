c57464b4102dc2b67eec56158f87736f26bb3887
raycast5 <ray.j.colon@gmail.com>
2022-07-11 03:05:01 +0000

    1st version

---
+++ _printf.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _printf(const char *const format, ...)
+{
+	va_list args;
+	int i, count = 0, flag = 0;
+	int (*func)(va_list);
+	
+	va_start(args, format);
+	
+	if(format)
+	{
+		for (i = 0; format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				i++;
+				func = matchf(format[i]);
+				if (func)
+				{
+					count += func(args);
+				}
+				else
+				{
+					_putchar(format[i - 1]);
+					count++;
+				}
+			}
+			else
+			{
+				_putchar(format[i]);
+				count++;
+			}
+		}
+	}
+va_end(args);
+return(count);
+}
